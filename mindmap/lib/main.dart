0df54680ca7be8a0616e8ca2831df803caad83d2
Neeraj Sawant <sawantneeraj04@gmail.com>
2024-11-05 13:25:33 +0530

    OK

--- mindmap/lib/main.dart
+++ mindmap/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/node.dart';
 import 'pages/mind_map_page.dart';
 
 void main() {
@@ -12,6 +13,10 @@ class MyApp extends StatelessWidget {
       title: 'Mind Mapping Tool',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          bodyText1: TextStyle(fontSize: 18.0, color: Colors.black),
+          bodyText2: TextStyle(fontSize: 16.0, color: Colors.black54),
+        ),
       ),
       home: MindMapPage(),
     );
